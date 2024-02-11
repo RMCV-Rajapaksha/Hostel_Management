327b3079da015e74fb9f671cd875a3d6b77799e1
RMCV-Rajapaksha <147098368+RMCV-Rajapaksha@users.noreply.github.com>
2024-02-11 23:39:36 +0530

    fill grid view and combobox

--- Hostel_Management/Fees.cs
+++ Hostel_Management/Fees.cs
@@ -16,9 +16,46 @@ namespace Hostel_Management
         public Fees()
         {
             InitializeComponent();
+            FillDGV();
+            FillRoomCombobox();
         }
         SqlConnection Con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\ROG\OneDrive\Documents\HostelMgmt.mdf;Integrated Security=True;Connect Timeout=30");
 
+        void FillDGV()
+        {
+            try
+            {
+                Con.Open();
+                string myquery = "Select * from Fees_tbl"; // Corrected the SQL query
+                SqlDataAdapter da = new SqlDataAdapter(myquery, Con);
+                var ds = new DataSet();
+                da.Fill(ds, "Fees_tbl"); // Added the table name to fill the DataSet correctly
+
+                dataGridView1.DataSource = ds.Tables[0]; // Corrected DataSorcery to DataSource
+
+                Con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        void FillRoomCombobox()
+        {
+            Con.Open();
+            String query = "Select * from Room_tbl where RoomStatus='" + "Active" + "'and Booked ='" + "Free" + "'";
+            SqlCommand cmd = new SqlCommand(query, Con);
+            SqlDataReader rdr;
+            rdr = cmd.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Columns.Add("RoomNum", typeof(int));
+            dt.Load(rdr);
+            comboBox1.ValueMember = "RoomNum";
+            comboBox1.DataSource = dt;
+
+            Con.Close();
+        }
         private void label5_Click(object sender, EventArgs e)
         {
             Application.Exit();
@@ -52,16 +89,17 @@ namespace Hostel_Management
             try
             {
                 Con.Open();
-                string query = "INSERT INTO Fees_tbl VALUES (@PaymentId, @StudentUSN, @Studentname,@StdRoom,@PaymntMonth,@College,@Amount )";
+                string query = "INSERT INTO Fees_tbl VALUES (@PaymentId, @StudentUSN, @Studentname,@StdRoom,@PaymntMonth,@Amount )";
                 SqlCommand cmd = new SqlCommand(query, Con);
                 cmd.Parameters.AddWithValue("@PaymentId", PaymentID.Text);
                 cmd.Parameters.AddWithValue("@StudentUSN", comboBox3.Text);
                 cmd.Parameters.AddWithValue("@Studentname", maskedTextBox2.Text);
                 cmd.Parameters.AddWithValue("@StdRoom", comboBox1.Text);
                 cmd.Parameters.AddWithValue("@PaymntMonth", dateTimePicker1.Text);
-                cmd.Parameters.AddWithValue("@College", maskedTextBox3.Text);
+                cmd.Parameters.AddWithValue("@Amount", maskedTextBox3.Text);
                 cmd.ExecuteNonQuery();
                 MessageBox.Show("Student Successfully Added");
+                FillDGV();
             }
             catch (Exception ex)
             {
